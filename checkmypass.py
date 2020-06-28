1288d19c94d338ad14aac8e00b6c5154ce16e163
Vu Pham <vdpham326@gmail.com>
2020-06-27 22:02:55 -0700

    add codes for password checker

---
+++ checkmypass.py
@@ -0,0 +1,46 @@
+import requests
+import hashlib
+import sys
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again')
+    return res
+
+
+def get_password_leaks_count(hashes, hash_to_check):
+    #split the hash into tuples delimited by : using list comprehensions
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count #return how many times pw has been leaked
+    return 0
+
+        
+
+
+def pwned_api_check(password):
+    # check password if it exists in the API response
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    #store the first 5 char in the 1st variable and the remaining char in tail variable
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    print(first5_char, tail)
+
+    return get_password_leaks_count(response, tail)
+    
+
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password) 
+        if count:
+            print(f'{password} wss found {count} times... you should probably change your password!')
+        else:
+            print(f'{password} was NOT found. Carry on!')
+    return 'done!'
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
